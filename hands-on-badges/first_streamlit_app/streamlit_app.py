56bf5f80925650bbf0bc9de010a854c73efcdc33
sarthakc44 <92016609+sarthakc44@users.noreply.github.com>
2023-05-09 18:57:45 +0530

    Update streamlit_app.py
    filter df as per multiselect

--- hands-on-badges/first_streamlit_app/streamlit_app.py
+++ hands-on-badges/first_streamlit_app/streamlit_app.py
@@ -16,4 +16,5 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
