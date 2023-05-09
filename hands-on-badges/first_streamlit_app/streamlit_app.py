e34cb1360f44092917ac6c24cac73274d727c1cd
sarthakc44 <92016609+sarthakc44@users.noreply.github.com>
2023-05-10 00:05:32 +0530

    Update streamlit_app.py
    added input text box for fruit choice

--- hands-on-badges/first_streamlit_app/streamlit_app.py
+++ hands-on-badges/first_streamlit_app/streamlit_app.py
@@ -19,9 +19,12 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+# input textbox with default value as Kiwi
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 # streamlit.text(fruityvice_response.json()) #only json
 
 # normalize the json response
