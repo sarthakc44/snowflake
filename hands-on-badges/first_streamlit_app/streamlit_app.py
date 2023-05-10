b3b24f6b31c4452ed1bea32887edb69d58518921
sarthakc44 <92016609+sarthakc44@users.noreply.github.com>
2023-05-11 00:32:45 +0530

    Update streamlit_app.py
    syntax error

--- hands-on-badges/first_streamlit_app/streamlit_app.py
+++ hands-on-badges/first_streamlit_app/streamlit_app.py
@@ -56,7 +56,7 @@ def get_fruit_load_list():
 
 streamlit.header("The fruit load list contains:")
 #button to load fruit list
-if streamlitbutton('Get Fruit Load List'):           
+if streamlit.button('Get Fruit Load List'):           
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
