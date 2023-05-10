de05371fea0294d36a276683fd4052c0641f7211
sarthakc44 <92016609+sarthakc44@users.noreply.github.com>
2023-05-11 00:09:45 +0530

    Update streamlit_app.py
    error handling for 1st input text box

--- hands-on-badges/first_streamlit_app/streamlit_app.py
+++ hands-on-badges/first_streamlit_app/streamlit_app.py
@@ -20,19 +20,26 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-# input textbox with default value as Kiwi
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+
+
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-# streamlit.text(fruityvice_response.json()) #only json
+try:
+  # input textbox with default value as Kiwi
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  # streamlit.text(fruityvice_response.json()) #only json
+  if not fruit_choice:
+    streamlit.error("Please enter a fruit to get information!")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    # normalize the json response
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # output normalized response as df
+    streamlit.dataframe(fruityvice_normalized)
 
-# normalize the json response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output normalized response as df
-streamlit.dataframe(fruityvice_normalized)
-
+except URLError as e:
+  streamlit.error()
+    
 #troubleshoot
 streamlit.stop()
 
