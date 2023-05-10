19ec3b05f880209f30968aadaed18bd3e8440a7c
sarthakc44 <92016609+sarthakc44@users.noreply.github.com>
2023-05-11 00:39:04 +0530

    Update streamlit_app.py
    syntax error

--- hands-on-badges/first_streamlit_app/streamlit_app.py
+++ hands-on-badges/first_streamlit_app/streamlit_app.py
@@ -28,7 +28,6 @@ def get_fruityvice_data(this_fruit_choice):
     return fruityvice_normalized
 
 
-
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -47,12 +46,14 @@ except URLError as e:
     
 
 streamlit.header("The fruit load list contains:")
+
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
-        return my_cur.fetchall(
-            
+        return my_cur.fetchall()
+
 #button to load fruit list
+
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
